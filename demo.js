c4796fca7fbcca050d67f801e8e443ee818893c8
Fardin Kibria <fardin_kibria@tjxcanada.ca>
2021-10-28 08:50:30 -0400

    new vibe

--- demo.js
+++ demo.js
@@ -1,6 +1,25 @@
 'use strict'; 
 (function() {
 
-  display('Hello World');
+  let person = {
+    firstName: 'Jim',
+    lastName: 'Cooper',
+    age = 17,
+    isAdult: function(){return this.age >=18}
+  };
+
+  function registerUser(fName, lName) {
+    let person = {
+      firstName,
+      lastName
+    };
+    display(person);
+  }
+
+  registerUser('Kris', 'Cooper');
+
+  //Objects.keys() prints out the contents of an object as an array
+
+  //Object.assign() allows copying content from one object to another.
 
 })();
